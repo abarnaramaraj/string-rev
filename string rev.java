adba3d3c5b0aa13808e95f2867e078b7f66cd4d4
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-07 12:44:26 +0530

    Update string rev.java

--- string rev.java
+++ string rev.java
@@ -6,7 +6,6 @@ public static void main(String[] args)
 {
 String name,reverse="";
 Scanner sc=new Scanner(System.in);
-System.out.println("enter the string");
 name=sc.nextLine();
 int length=name.length();
 for(int i=length-1;i>=0;i--)
